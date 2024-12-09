ec039ae126464b6f63250308d3995d17d6b44cbb
ulvirasulow <ulvirasul1@gmail.com>
2024-12-10 03:35:13 +0400

    half

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+
+        [JsonProperty]
+        public Guid Id{ get; private set; }
+
+        [JsonProperty]
+        public DateTime CreatedTime { get; private set; }
+
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedTime = DateTime.Now;
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createdTime)
+        {
+            Id = id;
+            CreatedTime = createdTime;
+        }
+
+        
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubManagers/InMemoryEventBusSubscriptionManager.cs
@@ -0,0 +1,66 @@
+﻿using EventBus.Base.Abstraction;
+using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.SubManagers
+{
+    public class InMemoryEventBusSubscriptionManager : IEventBusSubscriptionManager
+    {
+        public bool IsEmpty => throw new NotImplementedException();
+
+        public event EventHandler<string> OnEventRemoved;
+
+        public void AddSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEventKey<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetEventTypeByName(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasSubscriptionsForEvent(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
